61970b531dee0a02c02220d3091390d7aae802a1
bquinch <b.quinchon@talentoday.com>
2021-01-16 16:15:32 +0100

    [C] Install rspec workflow w/ rubocop

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe MessagesController, type: :controller do
   let(:message_params) { { from: "basile.quinchon@gmail.com", name: "Basile", subject: "Lolo", content: "Lala" } }
