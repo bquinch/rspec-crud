fba32f73e04804e7b28f1cfbf373840801fcc52e
bquinch <b.quinchon@talentoday.com>
2021-01-16 16:24:49 +0100

    [B] Fix rubocop double quotes

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Users Index View", type: :view do
   let(:user) { create(:user) }
-  it 'renders the users' do
+  it "renders the users" do
     assign(:users, [user])
     render template: "users/index.html.erb"
     expect(rendered).to include(user.name)
